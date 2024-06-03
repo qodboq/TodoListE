7c866322f3aabc3bc5a3cd4b52b1f953a1488f8b
qodboq <102157888+qodboq@users.noreply.github.com>
2024-06-03 11:50:26 +0200

    Initial Commit

---
+++ TodoListE/TodoListEApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListEApp.swift
+//  TodoListE
+//
+//  Created by Erik Valigurský on 03/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct TodoListEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
